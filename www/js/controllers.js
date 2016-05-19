1f97216c54fde0bf27d6befb5a9b1fc875a4cd57
joezneba <jonathanneira2012@gmail.com>
2016-05-19 15:46:35 -0500

    Prototipo Radio Unl
    Radio Unl_1

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('radioUniversitariaCtrl', function($scope) {
+
+})
+      
+.controller('quienesSomosCtrl', function($scope) {
+
+})
+   
+.controller('contactosCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.radioUniversitaria', {
+    url: '/inicio',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/radioUniversitaria.html',
+        controller: 'radioUniversitariaCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('menu.quienesSomos', {
+    url: '/quienesSomos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/quienesSomos.html',
+        controller: 'quienesSomosCtrl'
+      }
+    }
+  })
+
+  .state('menu.contactos', {
+    url: '/contactos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/contactos.html',
+        controller: 'contactosCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu/inicio')
+
+  
+
+});
